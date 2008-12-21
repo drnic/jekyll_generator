fe08d99218bf5548fade62c200ceff8dc14a48fe
Dr Nic Williams <drnicwilliams@gmail.com>
2008-12-22 06:51:47 +1000

    fixed manifest; bump to 1.0.1

--- lib/jekyll_generator.rb
+++ lib/jekyll_generator.rb
@@ -2,7 +2,7 @@ $:.unshift(File.dirname(__FILE__)) unless
   $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
 
 module JekyllGenerator
-  VERSION = '1.0.0'
+  VERSION = '1.0.1'
 end
 
 begin
