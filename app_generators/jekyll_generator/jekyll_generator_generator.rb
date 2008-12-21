25348de6416a9fe0edee4cd9cb6ae104138b05fb
Dr Nic Williams <drnicwilliams@gmail.com>
2008-12-21 15:58:16 +1000

    supported arguments are --author, --email, --title

--- app_generators/jekyll_generator/jekyll_generator_generator.rb
+++ app_generators/jekyll_generator/jekyll_generator_generator.rb
@@ -1,17 +1,15 @@
 class JekyllGeneratorGenerator < RubiGen::Base
 
-  DEFAULT_SHEBANG = File.join(Config::CONFIG['bindir'],
-                              Config::CONFIG['ruby_install_name'])
+  default_options :author => 'FIXME', :email => 'FIXME'
 
-  default_options :author => nil
-
-  attr_reader :name
+  attr_reader :author, :email, :title, :name
 
   def initialize(runtime_args, runtime_options = {})
     super
     usage if args.empty?
     @destination_root = File.expand_path(args.shift)
-    @name = base_name
+    @name             = base_name
+    @title            = base_name.humanize
     extract_options
   end
 
@@ -26,18 +24,15 @@ class JekyllGeneratorGenerator < RubiGen::Base
       # m.template_copy_each ["template.rb", "template2.rb"]
       # m.file     "file",         "some_file_copied"
       # m.file_copy_each ["path/to/file", "path/to/file2"]
-
-      m.dependency "install_rubigen_scripts", [destination_root, 'jekyll_generator'],
-        :shebang => options[:shebang], :collision => :force
     end
   end
 
   protected
     def banner
       <<-EOS
-Creates a ...
+Creates a Jekyell static website including Disqus comment system.
 
-USAGE: #{spec.name} name
+USAGE: #{spec.name} path/to/website
 EOS
     end
 
@@ -46,9 +41,15 @@ EOS
       opts.separator 'Options:'
       # For each option below, place the default
       # at the top of the file next to "default_options"
-      # opts.on("-a", "--author=\"Your Name\"", String,
-      #         "Some comment about this option",
-      #         "Default: none") { |options[:author]| }
+      opts.on("-a", "--author=\"Your Name\"", String,
+              "Your name will be pre-populated throughout website",
+              "Default: FIXME") { |v| options[:author] = v }
+      opts.on("-e", "--email=\"your@email.com\"", String,
+              "Your email will be pre-populated throughout website",
+              "Default: FIXME") { |v| options[:email] = v }
+      opts.on("-t", "--title=\"Your Project\"", String,
+              "Your project's human title",
+              "Default: current folder name") { |v| options[:title] = v }
       opts.on("-v", "--version", "Show the #{File.basename($0)} version number and quit.")
     end
 
@@ -62,10 +63,9 @@ EOS
     # Installation skeleton.  Intermediate directories are automatically
     # created so don't sweat their absence here.
     BASEDIRS = %w(
-      lib
-      log
-      script
-      test
-      tmp
+      _posts
+      layouts
+      css
+      images
     )
 end
